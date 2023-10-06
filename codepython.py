e7fa6c00c69ccbb6c9d6ccb8ca5e5b7703b2095e
xmunene <vmunene42@gmail.com>
2023-10-06 22:21:18 +0300

    Array Addition

--- codepython.py
+++ codepython.py
@@ -1,4 +1,2 @@
-course="""Python for Beginners. 
-This course allows students as well as enthusiasts to learn python. 
-By using notes and videos, learners can fully engage in the course."""
-print(course)
+a="Hello World!"
+print(a[1])
