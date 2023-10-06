093b0a398f42e012f7c697fce8f3f5a2f1964b66
xmunene <vmunene42@gmail.com>
2023-10-06 22:08:13 +0300

    Another Addition

--- codepython.py
+++ codepython.py
@@ -1,2 +1,4 @@
-course='Python for Beginners'
-print(course.upper())
+course="""Python for Beginners. 
+This course allows students as well as enthusiasts to learn python. 
+By using notes and videos, learners can fully engage in the course."""
+print(course)
